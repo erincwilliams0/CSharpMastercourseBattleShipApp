cfb0c9d22e9377dc125abafe023dfce09b71ac81
erin williams <erin.cwilliams0@gmail.com>
2025-06-11 16:25:10 -0500

    feat: created class and add properties

---
+++ BattleShipAppLibrary/Models/PlayerInfoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipAppLibrary.Models
+{
+    public class PlayerInfoModel
+    {
+        public string UserName { get; set; }
+        public List<GridSpotModel> ShipLocations { get; set; }
+        public List<GridSpotModel> HitLocations { get; set; }
+    }
+}
