f8fe92238f3a2a7a832cbd91a51b9df79467b181
erin williams <erin.cwilliams0@gmail.com>
2025-06-11 16:24:42 -0500

    feat: added ship status enum

---
+++ BattleShipAppLibrary/Models/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipAppLibrary.Models
+{
+    // 0 = empty, 1 = Ship, 2 = Miss, 3 = Hit, 4 = sunk
+    public enum GridSpotStatus
+    {
+        Empty,
+        Ship,
+        Miss,
+        Hit,
+        Sunk
+    }
+}
